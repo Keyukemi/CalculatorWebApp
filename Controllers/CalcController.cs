5e4fbdaced6e3fa52f2a7311232403f3a5390793
Keyukemi <keyukemi.ku@gmail.com>
2021-10-16 12:02:43 +0100

    The First Commit of my Calculator WebApp

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorWeb.Controllers 
+{
+    public class CalcController : Controller
+    {
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Add(string firstNumber, string secondNumber)
+        {
+            int numberOne = int.Parse(firstNumber);
+            int numberTwo = int.Parse(secondNumber);
+            int result = numberOne + numberTwo;
+
+            ViewBag.Result = result;
+
+            return View();
+        }
+
+    }
+}
